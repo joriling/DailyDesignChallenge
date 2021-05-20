d262c56d39c650f5abcb96567ba53b85efa54361
Jolly Ann Dolloso <jollyann_dolloso@ligph.com>
2021-05-20 10:46:34 +0800

    Add day 36 and say 37 challenge.

---
+++ Day36- Hoverboard/script.js
@@ -0,0 +1,27 @@
+const container = document.getElementById('container')
+const colors = ['#67D5B5', '#EE7785', '#ee6e9f', '#88dba3', '#cff0da', '#fbd14b', '#0080ff']
+const SQUARES = 500
+
+for (let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseout', () => removeColor(square))
+    container.appendChild(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.background = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.background = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
